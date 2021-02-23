6a5af2ef8ab0f98b29d89acc41d71124273f0a3e
Domnich Vitaly <dev.v.domnich@rainbowsoft.ru>
2021-02-23 20:00:04 +0400

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <span class="hidden-sm-and-up">
+        <!-- <v-toolbar-side-icon @click="sidebar = !sidebar"> </v-toolbar-side-icon> -->
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text v-for="item in menuItems" :key="item.title" :to="item.path">
+          <v-icon v-show="item.show" left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//import HelloWorld from "./components/HelloWorld";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  data() {
+    return {
+      ww: true,
+      appTitle: "Awesome App",
+      sidebar: false,
+    };
+  },
+  computed: {
+    menuItems() {
+      return [
+        { title: "Главная", path: "/", icon: "home" },
+        {
+          title: "Заключение договора",
+          path: "/about",
+          icon: "face",
+          show: true,
+        },
+        { title: "  Анализ", path: "/about", icon: "face", show: true },
+        { title: "Тех. поддержка", path: "/about", icon: "face", show: true },
+        { title: "Контакты", path: "/about", icon: "face", show: true },
+        { title: "О компании", path: "/about", icon: "face", show: true },
+        { title: "О методологии", path: "/about", icon: "face", show: true },
+        { title: "ЛК", path: "/about", icon: "face", show: true },
+        { title: "Войти", path: "/signin", icon: "lock_open", show: true },
+        {
+          title: "Управление пользователями",
+          path: "/signin",
+          icon: "lock_open",
+          show: true,
+        },
+      ];
+    },
+  },
+};
+</script>
+<style></style>
