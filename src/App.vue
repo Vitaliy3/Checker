f8604c7fbf0313fde1deb48436ca734e43fc74c9
Domnich Vitaly <dev.v.domnich@rainbowsoft.ru>
2021-03-04 01:16:11 +0400

    Добавлено меню с выпадающими элементами для тулбара

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,42 @@
           {{ appTitle }}
         </router-link>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn text v-for="item in menuItems" :key="item.title" :to="item.path">
-          <v-icon v-show="item.show" left dark>{{ item.icon }}</v-icon>
+        <v-btn text v-for="item in menuItems" :key="item.title" :to="item.path" v-show="item.show">
+          <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
+
+        <v-menu open-on-hover offset-y transition="slide-x-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text v-bind="attrs" v-on="on">
+              Тех. поддержка
+            </v-btn>
+          </template>
+          <v-list dense>
+            <v-list-item v-for="(item, index) in techSupport" :key="index" router :to="item.link">
+              <v-list-item-action>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-menu open-on-hover offset-y transition="slide-x-transition" offset-x>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text v-bind="attrs" v-on="on">
+              <v-icon large>mdi-account</v-icon>
+            </v-btn>
+          </template>
+          <v-list dense>
+            <v-list-item v-for="(item, index) in account" :key="index" router :to="item.link">
+              <v-list-item-action>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
       </v-toolbar-items>
     </v-app-bar>
 
@@ -40,7 +70,6 @@
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
@@ -49,6 +78,28 @@ export default {
       ww: true,
       appTitle: "Awesome App",
       sidebar: false,
+      techSupport: [{
+        title: "Инструкция",
+        link: "/instruction"
+      },
+        {
+          title: "Требования",
+          link: "/requirements"
+        },
+        {
+          title: "Общение",
+          link: "/communication"
+        }
+      ],
+      account: [{
+        title: "Личный кабинет",
+        link: "/privateOffice"
+      },
+        {
+          title: "Выйти из аккаунта",
+          link: "/logout"
+        }
+      ]
     };
   },
   computed: {
@@ -59,24 +110,16 @@ export default {
           title: "Заключение договора",
           path: "/about",
           icon: "face",
-          show: true,
+          show: true
         },
         { title: "  Анализ", path: "/about", icon: "face", show: true },
-        { title: "Тех. поддержка", path: "/about", icon: "face", show: true },
         { title: "Контакты", path: "/about", icon: "face", show: true },
         { title: "О компании", path: "/about", icon: "face", show: true },
         { title: "О методологии", path: "/about", icon: "face", show: true },
-        { title: "ЛК", path: "/about", icon: "face", show: true },
-        { title: "Войти", path: "/signin", icon: "lock_open", show: true },
-        {
-          title: "Управление пользователями",
-          path: "/signin",
-          icon: "lock_open",
-          show: true,
-        },
+        { title: "Войти", path: "/signIn", icon: "lock_open", show: true }
       ];
-    },
-  },
+    }
+  }
 };
 </script>
 <style></style>
